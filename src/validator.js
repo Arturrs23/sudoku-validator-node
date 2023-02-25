23d1d63a1aef56e40fce90c54fade26544e3457e
Arturs <arrturs.torsters@gmail.com>
2023-02-25 12:22:28 +0200

    5 success

--- src/validator.js
+++ src/validator.js
@@ -1,57 +1,68 @@
 class Validator {
   static validate(sudoku) {
-    const validator = new Validator
+    const validator = new Validator();
 
-    return validator.validate(sudoku)
+    return validator.validate(sudoku);
   }
-
-
-// If the puzzle is valid and completed, return Sudoku is valid.
-// If the puzzle is valid but not fully completed, return Sudoku is valid but incomplete.
-// If the puzzle is not valid, return Sudoku is invalid.
-
-
-// A puzzle is valid if:
-
-// No numbers are repeated in any of the rows
-// No numbers are repeated in any of the columns
-// Every 9x9 square has no repeated numbers
-// Only numbers 1 - 9 are used to fill the puzzle (or 0 for empty cells)
-
-//  0 - incomplete sudoku
-
-validate(sudoku) {
-  // loop rows to 9
-  for (let i = 0; i < 9; i++) { 
-
-    //  set() to check if there are any duplicate numbers
-    const row = new Set(sudoku[i]);
-
-    // use .size and if does not equal 9 unique elements stop and return
-    if (row.size !== 9 ) { 
-
-      // return different messages based on the condition
-      if (row.size == 9) {
-        return "Sudoku is valid.";
-      } else if (row.has(0)) {
-        return "Sudoku is valid but incomplete.";
-      } else {
-        return "Sudoku is invalid.";
-      }
-    }
-  }
+  validate(sudoku) {
+    const valid = "Sudoku is valid.";
+    const invalid = "Sudoku is invalid.";
+    const incomplete = "Sudoku is valid but incomplete.";
   
+    // clear the string
+    const newString = sudoku.replace(/[^\d]/g, "");
+
+  
+  
+  
+    // rows and cols
+    for (let i = 0; i < 9; i++) {
+      let row = "";
+      let column = "";
+  
+      for (let j = 0; j < 9; j++) {
+        // extract them to newString
+        row += newString.substr();
+        column += newString.substr(j * 9 + i, 1);
+      }
+      //using set() to check for duplicates
+      const rowSet = new Set(row);
+      const columnSet = new Set(column);
+        if ( columnSet.size !== 9 || rowSet.size !== 9) {
+          return invalid;
+      }
+
+    }
+
+ 
+
+  
+
+    // If Sudoku is valid and complete
+    return valid;
+
+    
+  }
 }
 
 
+module.exports = Validator;
 
 
 
 
 
-}
 
-module.exports = Validator
+
+
+
+
+
+
+
+
+
+
 
 
 
