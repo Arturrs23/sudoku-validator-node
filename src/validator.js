0cdc82e53f4de28a05b619c9457a7aeb0d52ffaa
Arturs <arrturs.torsters@gmail.com>
2023-02-27 10:47:53 +0200

    finished

--- src/validator.js
+++ src/validator.js
@@ -4,51 +4,72 @@ class Validator {
 
     return validator.validate(sudoku);
   }
+
   validate(sudoku) {
-    const valid = "Sudoku is valid.";
-    const invalid = "Sudoku is invalid.";
-    const incomplete = "Sudoku is valid but incomplete.";
-  
-    // clear the string
-    const newString = sudoku.replace(/[^\d]/g, "");
-
-  
-  
-  
-    // rows and cols
-    for (let i = 0; i < 9; i++) {
-      let row = "";
-      let column = "";
-  
-      for (let j = 0; j < 9; j++) {
-        // extract them to newString
-        row += newString.substr();
-        column += newString.substr(j * 9 + i, 1);
-      }
-      //using set() to check for duplicates
-      const rowSet = new Set(row);
-      const columnSet = new Set(column);
-        if ( columnSet.size !== 9 || rowSet.size !== 9) {
-          return invalid;
-      }
-
-    }
-
- 
-
-  
-
-    // If Sudoku is valid and complete
-    return valid;
-
+    // sudoku to digits array
+    const digits = sudoku.match(/\d/g);
     
+    // log the arr
+    console.log(digits)
+  
+    // if no numbers or doesnt have 81 digits
+    if (!digits || digits.length !== 81) {
+      return 'Sudoku is invalid.';
+    }
+  
+    // arrays for cols rows and 3x3 fields
+    const rows = [];
+    const cols = [];
+    const fields = [];
+  
+    // creating new set() to arrays 
+    // set to check unique digits
+    for (let i = 0; i < 9; i++) {
+      rows.push(new Set());
+      cols.push(new Set());
+      fields.push(new Set());
+    }
+  
+    // iterating over digits
+    for (let i = 0; i < digits.length; i++) {
+      const digit = digits[i];
+      const row = Math.floor(i / 9);
+      const col = i % 9;
+      const field = Math.floor(row / 3) * 3 + Math.floor(col / 3);
+  
+      //if there is zero
+      if (digit === '0') {
+        continue;
+      }
+  
+      // if there are duplicates in row col or 3x3 field then invalid
+      if (rows[row].has(digit) || cols[col].has(digit) || fields[field].has(digit)) {
+        return 'Sudoku is invalid.';
+      }
+  
+      // if no duplicates then add to the new set()
+      rows[row].add(digit);
+      cols[col].add(digit);
+      fields[field].add(digit);
+    }
+  
+    // 
+    const emptyField = digits.filter(digit => digit === '0').length;
+  
+    // check if sudoku is valid but incomplete
+    if (emptyField > 0 && emptyField > 10) {
+      return 'Sudoku is valid but incomplete.';
+    }
+  
+
+
+    return 'Sudoku is valid.';
   }
 }
+  
 
 
 module.exports = Validator;
-
-
 
 
 
